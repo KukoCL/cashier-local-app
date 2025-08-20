f27a26ca3fc0c9b34fd04865b9e27a577011f629
KukoCL <webphantom.hc@gmail.com>
2025-08-19 20:54:12 -0400

    fixes

--- test-seed.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Cleaning database and testing seed system...
-if exist data.db del data.db
-if exist bin\Debug\net9.0\data.db del bin\Debug\net9.0\data.db
-echo Starting application with fresh database...
-dotnet run --project ElectronApp.csproj
